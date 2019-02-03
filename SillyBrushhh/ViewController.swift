f8b4de84d96bafdb06c3504ad6f0e15cb5f641fb
sinahk <sinahk1992@yahoo.com>
2019-02-03 17:14:32 +0330

    Initial Commit

---
+++ SillyBrushhh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SillyBrushhh
+//
+//  Created by sinahk on 2/3/19.
+//  Copyright © 2019 sinakhodadad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SillyBrushhhTests/SillyBrushhhTests.swift
@@ -0,0 +1,34 @@
+//
+//  SillyBrushhhTests.swift
+//  SillyBrushhhTests
+//
+//  Created by sinahk on 2/3/19.
+//  Copyright © 2019 sinakhodadad. All rights reserved.
+//
+
+import XCTest
+@testable import SillyBrushhh
+
+class SillyBrushhhTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
